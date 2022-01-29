5dfa715854c98395a2ce14aa28b3d8e8acc80679
ryanparker196 <ryanparker196@gmail.com>
2022-01-29 15:53:51 -0500

    finished multi-joints

--- generate.py
+++ generate.py
@@ -1,7 +1,22 @@
+from email import header
 import pyrosim.pyrosim as pyrosim
 
-pyrosim.Start_SDF("box.sdf")
+pyrosim.Start_SDF("boxes.sdf")
 
-pyrosim.Send_Cube(name="Box", pos=[0,0,0.5] , size=[1,1,1])
+
+# pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length, width, height])
+# pyrosim.Send_Cube(name="Box2", pos=[x+1,y,z+1] , size=[length, width, height])
+
+def buildStack(i :int, xPos :int, yPos :int):
+    [length, width, height] = [1, 1, 1]
+    z = .5
+    for i in range(10):
+        pyrosim.Send_Cube(name="Box_{}".format(i), pos=[xPos,yPos,z] , size=[length, width, height])
+        [length, width, height] = [length*.9, width*.9, height*.9]
+        z = z + height + .5
+
+for row in range(4):
+    for col in range(4):
+        buildStack(10, row, col)
 
 pyrosim.End()
--- simulation.py
+++ simulation.py
@@ -1,12 +1,16 @@
 import pybullet as p
+import pybullet_data
 import time
 
 physicsClient = p.connect(p.GUI)
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
 
-p.loadSDF("box.sdf")
+p.setGravity(0,0,-9.8)
+planeId = p.loadURDF("plane.urdf")
+p.loadSDF("boxes.sdf")
 
 for x in range(1000):
-    print(x)
+    print("step {}".format(x))
     time.sleep(1/60)
     p.stepSimulation()
 
