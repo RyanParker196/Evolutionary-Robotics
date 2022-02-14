8fbb5c6f22c9bb760f2fcd9a433dc8b53266d3f0
ryan <RyanParker196@gmail.com>
2022-02-14 13:45:27 -0500

    random movement

--- simulation.py
+++ simulation.py
@@ -3,6 +3,7 @@ import pybullet_data
 import pyrosim.pyrosim as pyrosim
 import numpy as np
 import time
+import random
 
 physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
@@ -13,7 +14,7 @@ robotId = p.loadURDF("body.urdf")
 worldId = p.loadSDF("world.sdf")
 pyrosim.Prepare_To_Simulate(robotId)
 
-numSteps = 1000
+numSteps = 10000
 torsoValues = np.zeros(numSteps)
 backLegValues = np.zeros(numSteps)
 
@@ -22,6 +23,15 @@ for i in range(numSteps):
     p.stepSimulation()
     torsoValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("Torso")
     backLegValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
+
+    pos1 = random.random() * (np.pi / 2)
+    pyrosim.Set_Motor_For_Joint(bodyIndex=robotId, jointName="BackLeg_Torso",
+        controlMode=p.POSITION_CONTROL, targetPosition=pos1, maxForce=500)
+
+    pos2 = random.random() * (np.pi / 2)
+    pyrosim.Set_Motor_For_Joint(bodyIndex=robotId, jointName="Torso_FrontLeg",
+        controlMode=p.POSITION_CONTROL, targetPosition=pos2, maxForce=500)
+
 
 np.save("data/torsoSensorValues.npy", torsoValues)
 np.save("data/backLegSensorValues.npy", backLegValues)
